f8bf823c4d1d23480b165616eeeadf2091351572
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-14 16:28:18 -0300

    create route to get all prompts and upload video

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from "fastify"
 
+import { uploadVideo } from "./routes/upload-video"
+import { getAllPrompts } from "./routes/get-all-prompts"
+
 const app = fastify()
 
-app.get("/", () => {
-  return "Hello world"
-})
+app.register(uploadVideo)
+app.register(getAllPrompts)
 
 app
   .listen({
