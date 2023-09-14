27e5f841beb43227c4fc6ba8a6d79d4cd7d35d80
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-14 16:59:36 -0300

    create route to create transcription video

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,13 @@ import { fastify } from "fastify"
 
 import { uploadVideo } from "./routes/upload-video"
 import { getAllPrompts } from "./routes/get-all-prompts"
+import { createTranscription } from "./routes/create-transcription"
 
 const app = fastify()
 
 app.register(uploadVideo)
 app.register(getAllPrompts)
+app.register(createTranscription)
 
 app
   .listen({
