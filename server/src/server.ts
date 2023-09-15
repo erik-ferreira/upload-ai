1536cdfb6655cfec95ac93ba0935b3f35cd27c10
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-14 22:39:53 -0300

    create route generate ai completion

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,19 @@ import { fastify } from "fastify"
 import { uploadVideo } from "./routes/upload-video"
 import { getAllPrompts } from "./routes/get-all-prompts"
 import { createTranscription } from "./routes/create-transcription"
+import { generateAiCompletion } from "./routes/generate-ai-completion"
 
 const app = fastify()
 
 app.register(uploadVideo)
 app.register(getAllPrompts)
 app.register(createTranscription)
+app.register(generateAiCompletion)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => console.log("Http server running"))
+
+// 1:06:00
